15ceb9441bc169d996564dde1fb8d8dadef4a95d
joshcarp <joshuacarpeggiani@gmail.com>
2020-04-06 21:21:46 +1000

    Add Makefile

--- main.go
+++ main.go
@@ -9,19 +9,27 @@ import (
 )
 
 type Server struct {
-	http.Handler
-	verbose bool
+	Hosts map[string]http.Handler
+	Verbose bool
 }
 
-
 func main() {
-	s := Server{Handler: http.FileServer(http.Dir("./web1.comp30023"))}
 
-	// Parse verbose mode
-	flag.BoolVar(&s.verbose, "v", false, "Verbose mode: Prints out request")
+	// Parse Verbose mode
+	var verbose bool
+	flag.BoolVar(&verbose, "v", false, "Verbose mode: Prints out request")
 	flag.Parse()
 
+	serveHost(verbose, "web1.comp30023", "web2.comp30023", "web3.comp30023")
+}
+
+func serveHost(v bool, hosts ...string ){
 	// Start up a server
+	s := Server{Verbose: v}
+	s.Hosts = make(map[string]http.Handler)
+	for _, host := range hosts{
+		s.Hosts[host] = http.FileServer(http.Dir(host))
+	}
 	log.Println("Listening on :80...")
 	err := http.ListenAndServe(":80", s)
 	if err != nil {
@@ -32,10 +40,11 @@ func main() {
 //ServeHTTP writes out debug info to stdout and redirects the request to the filesystem handler
 func (s Server)ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Visited URL: %s\n",r.URL)
-	if s.verbose{
+	if s.Verbose {
 		byts, _ := httputil.DumpRequest(r, true)
 		fmt.Printf("Request sent: %s\n\n",string(byts))
 	}
 	// Forward the response onto the filesystem handler
-	s.Handler.ServeHTTP(w,r)
+	println(r.Host)
+	s.Hosts[r.Host].ServeHTTP(w,r)
 }
